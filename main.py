95014d924ff54e97a868201c643019bfe2c8859b
Somgr <deniska9767@gmail.com>
2023-04-12 09:50:53 +0500

    upd

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from fake_useragent import UserAgent
 
 ua = UserAgent()
 
+
 def get_job_links(url):
     headers = {
         "Accept": "text/css,*/*;q=0.1",
@@ -27,26 +28,28 @@ def get_job_links(url):
 
     return job_links
 
+
 def get_info_from_links(job_links):
     counter = 0
     for link in job_links:
         url = link
         driver = webdriver.Chrome(
-            executable_path=r'C:\Users\Dean\Desktop\py_proj\1\olx_parser\chromedriver\chromedriver.exe')
+            executable_path=r'C:\Users\Dean\Desktop\Projects\olx_parser\chromedriver.exe')
 
         try:
             driver.get(url=link)
+            time.sleep(2)
             coockies = driver.find_element(By.XPATH, '//*[@id="root"]/div[3]/div[3]/button/span/span')
-            coockies.click()
-
-            title = driver.find_element(By.CLASS_NAME, 'css-r9zjja-Text').text
-            descr = driver.find_element(By.CLASS_NAME, 'css-1g2c8kb-Text').text
-            phone_element = driver.find_element(By.CLASS_NAME, 'css-cuxnr-BaseStyles')
             time.sleep(1)
+            coockies.click()
+            time.sleep(1)
+            phone_element = driver.find_element(By.CLASS_NAME, 'css-1nd8q08').find_element(By.TAG_NAME, 'span')
+            time.sleep(2)
             phone_element.click()
             time.sleep(4)
             phone_number = driver.find_element(By.CSS_SELECTOR, 'a.css-v1ndtc').text
 
+            phone_number = phone_number.replace('tel:', '')
             phone_number = phone_number.replace('+', '')
             phone_number = ''.join(phone_number.split('-'))
             phone_number = ''.join(phone_number.split())
@@ -56,6 +59,11 @@ def get_info_from_links(job_links):
                 pass
 
             phone_number = '+' + phone_number
+
+            title = driver.find_element(By.CLASS_NAME, 'css-tcqyb').text
+            time.sleep(1)
+            descr = driver.find_element(By.CLASS_NAME, 'css-19srbbu').text
+            time.sleep(1)
 
             with open('vacancy.txt', 'a', encoding='utf-8') as file:
                 file.write(f'#Вакансия\n\n{title}\n\n{descr}\n\nТелефон: {phone_number}\n\n**************\n\n')
@@ -72,6 +80,8 @@ def get_info_from_links(job_links):
             driver.close()
             driver.quit()
 
+
 if __name__ == '__main__':
-    links = get_job_links(url=input("Enter url in olx: "))
+    # links = get_job_links(url=input("Enter url in olx: "))
+    links = get_job_links('https://www.olx.uz/d/rabota/samarkand/')
     get_info_from_links(links)
